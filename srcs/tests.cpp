0797c04a08e0683fbc4a9160818abc02cab342ac
DailyWind00 <gallaismael0@gmail.com>
2025-10-10 06:16:16 +0200

    Add test cases & description

--- srcs/tests.cpp
+++ srcs/tests.cpp
@@ -11,19 +11,19 @@ TEST_CASE("Ex00 Vector & Matrix add/sub/scl") {
 
 	vec1.add(vec2);
 	CHECK(vec1 == Vector<f32>({5, 7, 9}));
-	CHECK_THROWS(vec1.add(Vector<f32>({1, 2})));
+	CHECK_THROWS(vec1.add(Vector<f32>({1, 2}))); // Mismatched sizes
 	vec1.sub(vec2);
 	CHECK(vec1 == Vector<f32>({1, 2, 3}));
-	CHECK_THROWS(vec1.sub(Vector<f32>({1, 2})));
+	CHECK_THROWS(vec1.sub(Vector<f32>({1, 2}))); // Mismatched sizes
 	vec1.scl(2.0f);
 	CHECK(vec1 == Vector<f32>({2, 4, 6}));
 
 	mat1.add(mat2);
 	CHECK(mat1 == Matrix<f32>({{8, 10, 12}, {14, 16, 18}}));
-	CHECK_THROWS(mat1.add(Matrix<f32>({{1, 2}, {3, 4}})));
+	CHECK_THROWS(mat1.add(Matrix<f32>({{1, 2}, {3, 4}}))); // Mismatched shapes
 	mat1.sub(mat2);
 	CHECK(mat1 == Matrix<f32>({{1, 2, 3}, {4, 5, 6}}));
-	CHECK_THROWS(mat1.sub(Matrix<f32>({{1, 2}, {3, 4}})));
+	CHECK_THROWS(mat1.sub(Matrix<f32>({{1, 2}, {3, 4}}))); // Mismatched shapes
 	mat1.scl(2.0f);
 	CHECK(mat1 == Matrix<f32>({{2, 4, 6}, {8, 10, 12}}));
 }
@@ -36,8 +36,11 @@ TEST_CASE("Ex01 linear_combination") {
 	Vector<f32> vec4 = {1, 2, 3};
 	Vector<f32> vec5 = {0, 10, -100};
 
+	Vector<f32> vec6 = {1, 2};
+
 	CHECK(linear_combination({vec1, vec2, vec3}, {10.0f, -2.0f, 0.5f}) == Vector<f32>({10, -2, 0.5f}));
 	CHECK(linear_combination({vec4, vec5}, {10.0f, -2.0f}) == Vector<f32>{10.0f, 0.0f, 230.0f});
 	CHECK(linear_combination<f32>({}, {}) == Vector<f32>{});
-	CHECK_THROWS(linear_combination({vec1}, {}));
+	CHECK_THROWS(linear_combination({vec1}, {})); // Mismatched sizes
+	CHECK_THROWS(linear_combination({vec1, vec6}, {1.0f, 2.0f})); // Mismatched vector sizes
 }
